1cf02d7470fb2efe7cc6b1627d0282b09df8fad6
AKSAHA <sheol.aks@gmail.com>
2019-02-08 16:36:37 +0530

    ContentChild logic added

--- src/app/decorators/contentchild/tabs/tabs.component.ts
+++ src/app/decorators/contentchild/tabs/tabs.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChild, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss']
 })
-export class TabsComponent implements OnInit {
+export class TabsComponent implements OnInit, AfterContentInit {
 
   constructor() { }
+  @ContentChild('divElement') div: any;
 
   ngOnInit() {
   }
+
+ ngAfterContentInit() {
+   console.log(this.div);
+ }
 
 }
