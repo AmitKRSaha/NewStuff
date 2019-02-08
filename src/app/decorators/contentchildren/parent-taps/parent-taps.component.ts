2ad05f10e1daba7057ab40575005fe28c7961351
AKSAHA <sheol.aks@gmail.com>
2019-02-08 16:55:19 +0530

    Added some msg

--- src/app/decorators/contentchildren/parent-taps/parent-taps.component.ts
+++ src/app/decorators/contentchildren/parent-taps/parent-taps.component.ts
@@ -12,4 +12,10 @@ export class ParentTapsComponent implements OnInit {
   ngOnInit() {
   }
 
+
+
+//   Things to Remember —
+// The QueryList is initialized only before the ngAfterContentInit lifecycle hook, therefore, is available only from this point.
+// ContentChildren includes only elements that exists within the ng-content tag.
+// Returns QueryList like ViewChildren .
 }
