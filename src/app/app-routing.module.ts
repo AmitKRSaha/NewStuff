11962bdc87ea84eb80ddfe8bf38e9546a140adba
AKSAHA <sheol.aks@gmail.com>
2019-02-20 12:49:19 +0530

    Fixed th bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {path: 'attribute', loadChildren: './decorators/decorators.module#DecoratorsModule'},
-  {path: 'grid', loadChildren: './grid/grid.module#GridModule'},
+  // {path: 'grid', loadChildren: './grid/grid.module#GridModule'},
 
 ];
 
